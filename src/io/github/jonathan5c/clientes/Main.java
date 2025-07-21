186f44bf84a20bc3a14b6bc6a6b184e64bf4ed27
Jonathan Castro <jonathan.castro18@outlook.com>
2025-07-21 09:18:47 -0300

    Criando o pacote de domínio da aplicação.

--- src/io/github/jonathan5c/clientes/Main.java
+++ src/io/github/jonathan5c/clientes/Main.java
@@ -1,3 +1,5 @@
+package io.github.jonathan5c.clientes;
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/io/github/jonathan5c/clientes/dominio/Cliente.java
@@ -0,0 +1,56 @@
+package io.github.jonathan5c.clientes.dominio;
+
+import java.util.UUID;
+
+public class Cliente {
+
+    private UUID codigoCliente;
+    private String nome;
+    private String cpf;
+    private String sexo;
+    private byte[] foto;
+
+    public Cliente() {
+        this.codigoCliente = UUID.randomUUID();
+    }
+
+    public UUID getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(UUID codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto = foto;
+    }
+}
