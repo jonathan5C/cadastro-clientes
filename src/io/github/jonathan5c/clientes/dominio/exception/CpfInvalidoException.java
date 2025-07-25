5696b12946971287544a9a653c4a3a19d05a0b6a
Jonathan Castro <jonathan.castro18@outlook.com>
2025-07-25 08:53:24 -0300

    Criando a exception para validar o CPF do cliente.

---
+++ src/io/github/jonathan5c/clientes/dominio/exception/CpfInvalidoException.java
@@ -0,0 +1,7 @@
+package io.github.jonathan5c.clientes.dominio.exception;
+
+public class CpfInvalidoException extends Exception {
+    public CpfInvalidoException(String message) {
+        super(message);
+    }
+}
